01a0be4a130c3f938911f91b40f2a8eec84bbb4f
Laman Salimova <104246181+LemanSelimova@users.noreply.github.com>
2024-01-14 00:23:54 +0400

    add header

--- src/scenes/dashboard/Dashboard.jsx
+++ src/scenes/dashboard/Dashboard.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <Box m="20px">
+      {/* HEADER */}
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />;
+      </Box>
+    </Box>
+  );
 }
