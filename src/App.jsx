d6b1603a275b8d71002327770cb958054ed1e766
Laman Salimova <104246181+LemanSelimova@users.noreply.github.com>
2024-01-13 23:51:49 +0400

    add sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { routes } from "./utils/routes";
 
 export default function App() {
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar isSidebar={isSidebar} />
           <main className="content">
-            <Topbar />
+            <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
               {routes.map((route, index) => (
                 <Route
