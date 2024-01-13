0ceef5c259a5cc7340a43b386ec1e9498c2ae170
Laman Salimova <104246181+LemanSelimova@users.noreply.github.com>
2024-01-13 14:24:56 +0400

    add project  structure

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@material-ui/material";
+
+export default function App() {
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content"></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
