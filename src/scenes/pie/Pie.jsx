5852301a4205005c715db4df9e887aa5e8e777ca
Laman Salimova <104246181+LemanSelimova@users.noreply.github.com>
2024-01-20 14:47:07 +0400

    add Geography & Line & PieChart components

--- src/scenes/pie/Pie.jsx
+++ src/scenes/pie/Pie.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+import PieChart from "../../components/PieChart";
 
 export default function Pie() {
-  return <div>Pie</div>;
+  return (
+    <Box m="20px">
+      <Header title="Pie Chart" subtitle="Simple Pie Chart" />
+      <Box height="75vh">
+        <PieChart />
+      </Box>
+    </Box>
+  );
 }
